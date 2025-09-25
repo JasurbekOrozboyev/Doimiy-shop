840836bdc173a3b559ac2fbc47decc33f15a2ea1
JasurbekOrozboyev <orazboyev1jasurbek@gmail.com>
2025-09-25 20:56:47 +0500

    xabar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
+// onlayn dokon
 
 function App() {
   return (
